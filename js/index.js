055feab4a4b09500a8df07b59a668d5b62875918
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-05-18 15:31:31 -0300

    adds graphs

--- js/index.js
+++ js/index.js
@@ -22,10 +22,47 @@ function feelingsCsv(obj) {
   return header + '\n' + values;
 }
 
+function drawFeelingsGraph() {
+  const graphDiv = document.getElementById('my-graph-container');
+  graphDiv.innerHTML = '';
+
+  const total = Object.keys(window.feelings).reduce((acc, val) => {
+    return acc + window.feelings[val];
+  }, 0);
+
+  const inOrder = Object.keys(window.feelings).sort((a, b) => {
+    return window.feelings[b] - window.feelings[a];
+  });
+
+  inOrder.forEach((v) => {
+    const mRow = document.createElement('div');
+    const mLabel = document.createElement('div');
+    const mBar = document.createElement('div');
+
+    const pctValue = window.feelings[v] / total;
+    const pctLabel = Math.floor(pctValue * 100);
+    const pctWidth = Math.floor(pctValue * 83);
+
+    mRow.classList.add('graph-row');
+    mLabel.classList.add('graph-label');
+    mBar.classList.add('graph-bar');
+
+    mLabel.innerHTML = v;
+    mBar.innerHTML = pctLabel + '%';
+    mBar.style.width = pctWidth + '%';
+
+    mRow.appendChild(mLabel);
+    mRow.appendChild(mBar);
+
+    graphDiv.appendChild(mRow);
+  });
+}
+
 document.getElementById('start-button').addEventListener('click', function() {
   window.appRunning = !window.appRunning;
 
   document.getElementById('my-cam').style.opacity = window.appRunning ? '1' : '0';
+  document.getElementById('my-graph-container').style.opacity = window.appRunning ? '0' : '1';
   document.getElementById('save-button').style.display = window.appRunning ? 'none' : 'inline-block';
 
   this.innerHTML = window.appRunning ? 'Pausar' : 'Iniciar';
@@ -35,6 +72,7 @@ document.getElementById('start-button').addEventListener('click', function() {
   } else {
     clearCanvases();
     clearInterval(window.loopID);
+    drawFeelingsGraph();
   }
 }, false);
 
